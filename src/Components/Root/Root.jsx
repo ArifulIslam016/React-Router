002004ded7a0d6f1516df6538f8c3962b9fe332c
Ariful Islam <arifulq234@gmail.com>
2025-09-29 15:43:09 +0600

    navigator added

--- src/Components/Root/Root.jsx
+++ src/Components/Root/Root.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import { Outlet } from 'react-router';
+import { Outlet, useNavigation } from 'react-router';
 import Home from '../Home/Home';
 import Footer from '../Footer/Footer';
 import Sidebar from '../Sidebar/Sidebar';
 import './Root.css'
+
 const Root = () => {
+  const navigation= useNavigation()
+  const isNavigating= Boolean(navigation.location)
+  console.log(isNavigating)
     return (
       <div>
         <Home></Home>
-        <div className='flex'>
+        <div className="flex">
           <Sidebar></Sidebar>
+          {isNavigating && <h1>Loading...</h1>}
           <Outlet></Outlet>
         </div>
 
--- src/Components/SingleUser/UserDetails/UserDetails.jsx
+++ src/Components/SingleUser/UserDetails/UserDetails.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import { useLoaderData } from 'react-router';
+import { useLoaderData, useNavigate, useParams } from 'react-router';
 
 const UserDetails = () => {
     const UDetails=useLoaderData()
+    let Navigate= useNavigate()
+  const BacktoPrevious=()=>{
+    Navigate('/User')
+  }
+  const params= useParams()
+  console.log(params)
     return (
       <div>
         <h1>This is details Sections</h1>
         <div>
           <h1>{UDetails.website}</h1>
+          <button onClick={BacktoPrevious}>Go Home</button>
         </div>
       </div>
     );
